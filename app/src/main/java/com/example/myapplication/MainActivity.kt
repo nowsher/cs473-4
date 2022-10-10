57c45b077336a58db0638b413c8da6ee874196f7
nowsher <alinowsher@yahoo.com>
2022-10-09 20:06:37 -0500

    v2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -30,9 +30,6 @@ class MainActivity : AppCompatActivity() {
         editTextEmail.setText("nowsher@asd.asd")
         editTextPassword.setText("1234")
 
-//        var tmpView:View = findViewById(R.id.layoutParent)
-//        var tEditText:EditText = findViewById(R.id.editTextEmail)
-        //Log.i("xxx","yyy")
         //////////////////////////////
         buttonLogin.setOnClickListener {
 
@@ -69,34 +66,6 @@ class MainActivity : AppCompatActivity() {
             var userAccount: UserAccount? = userList.find { s -> s.userEmail == tmpEmail }
 
 
-
-//            //open Dial app
-//            val intent = Intent(Intent.ACTION_DIAL)
-//            intent.data = Uri.parse("tel:+16418192474")
-//            if (intent.resolveActivity(packageManager) != null) {
-//                startActivity(intent)
-//            }
-
-//            // open URL in browser
-//            val uri = Uri.parse("http://www.google.com")
-//            val it = Intent(Intent.ACTION_VIEW, uri)
-//            startActivity(it)
-
-//            //Map Iowa latitude and longitude
-//            val uri = Uri.parse("geo:41.01605390860477, -91.96702008827701") //41.01605390860477, -91.96702008827701 (old 41.8780,93.0977)
-//            val it = Intent(Intent.ACTION_VIEW, uri)
-//            startActivity(it)
-
-//            //open Email app
-//            val intent = Intent()
-//            intent.action = Intent.ACTION_SENDTO //Intent.ACTION_SEND // to send SMS
-//            intent.type = "*/*"
-//            intent.putExtra(Intent.EXTRA_EMAIL, "mohammad.ali@miu.edu")
-//            intent.putExtra(Intent.EXTRA_SUBJECT, "Check your password")
-//            intent.putExtra(Intent.EXTRA_TEXT, "Your password is :" + userAccount?.password)
-//            startActivity(intent)
-
-
             //Email
             val intent = Intent(Intent.ACTION_SENDTO).apply {
                 type = "*/*"    //"text/plain"
@@ -106,21 +75,6 @@ class MainActivity : AppCompatActivity() {
             }
             startActivity(intent)
 
-//            try {
-//                startActivity(Intent.createChooser(intent, "Send mail..."))
-//                finish()
-//                Toast.makeText(this@MainActivity,"Finished sending email...",Toast.LENGTH_LONG).show()
-//            } catch (ex: ActivityNotFoundException) {
-//                Toast.makeText(this@MainActivity,"There is no email client installed.",Toast.LENGTH_SHORT).show()
-//            }
-
-
-//            //SMS
-//            val intent = Intent()
-//            intent.action = Intent.ACTION_SEND
-//            intent.type = "text/plain"
-//            intent.putExtra(Intent.EXTRA_TEXT, "my message")
-//            startActivity(intent)
 
         }
 
