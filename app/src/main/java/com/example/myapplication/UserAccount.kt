f1a7f725d68eb4ff22c9fb3469ac788ff98b3658
nowsher <alinowsher@yahoo.com>
2022-10-15 22:46:20 -0500

    v4

--- app/src/main/java/com/example/myapplication/UserAccount.kt
+++ app/src/main/java/com/example/myapplication/UserAccount.kt
@@ -2,14 +2,20 @@ package com.example.myapplication
 
 import java.io.Serializable
 
-data class UserAccount (var id:Int = 0, var firstName:String? = "", var lastName:String? = "", var userEmail:String?, var password:String?)
+data class UserAccount (var id:Int = 0, var firstName:String? = "", var lastName:String? = ""
+                        , var userEmail:String?, var password:String?)
     : Serializable {
+
+    override fun toString(): String {
+        return "[$firstName , $lastName , $userEmail]"
+    }
 
     override fun equals(other: Any?): Boolean {
         var ua:UserAccount  = other as UserAccount
         if (ua.userEmail.isNullOrEmpty() && ua.password.isNullOrEmpty()){
             return false
         }
+
         if (ua.userEmail.equals(this.userEmail) && ua.password.equals(this.password)){
             return true
         }
